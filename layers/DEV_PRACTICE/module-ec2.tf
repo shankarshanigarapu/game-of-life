45a756666e269fc56cf5b3fc966b228a8fcbd155
mani5a3 <41730370+mani5a3@users.noreply.github.com>
2019-07-29 16:28:09 +0530

    Update module-ec2.tf

--- layers/DEV_PRACTICE/module-ec2.tf
+++ layers/DEV_PRACTICE/module-ec2.tf
@@ -1,8 +1,33 @@
-module "ec2" {
-  source = "../../modules/ec2"
-  ami             = "${var.ami}"
-  instance_type   = "${var.instance_type}"
-  key_name        = "${var.key_name}"
-  availability_zones= "${var.availability_zones}"
+module "asg" {
+  source = "../../modules/asg"
+  name_prefix          = "${var.name_prefix}"
+  image_id             = "${var.image_id}"
+  instance_type        = "${var.instance_type}"
+  key_name             = "${var.key_name}"
+  availability_zones   = "${var.availability_zones}"
+}
 
+  
+module "rds" {
+  source = "../../modules/rds"
+  engine               = "${var.engine}"
+  engine_version       = "${var.engine_version}"
+  instance_class       = "${var.instance_class}"
+  name                 = "${var.name}"
+  username             = "${var.username}"
+  password             = "${var.password}"
+  parameter_group_name = "${var.parameter_group_name}"
+  
+ 
+}
+
+module "dynamodb" {
+  source = "../../modules/dynamodb"
+  
+}
+
+
+module "s3withkms" {
+  source = "../../modules/s3withkms"
+  
 }
