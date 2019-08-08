6902c4c4c66583d347a30086c367f219df4db785
mani5a3 <41730370+mani5a3@users.noreply.github.com>
2019-08-08 15:10:57 +0530

    Update module-ec2.tf

--- layers/DEV_PRACTICE/module-ec2.tf
+++ layers/DEV_PRACTICE/module-ec2.tf
@@ -13,5 +13,28 @@ module "asg_and_alb" {
 }
 
 
+module "rds" {
+  source = "../../modules/rds"
+  engine               = "${var.engine}"
+  engine_version       = "${var.engine_version}"
+  instance_class       = "${var.instance_class}"
+  name                 = "${var.name}"
+  username             = "${var.username}"
+  password             = "${var.password}"
+  parameter_group_name = "${var.parameter_group_name}"
+  
+ 
+}
+
+module "dynamodb" {
+  source = "../../modules/dynamodb"
+  
+}
+
+
+module "s3withkms" {
+  source = "../../modules/s3withkms"
+  
+}
 
   
